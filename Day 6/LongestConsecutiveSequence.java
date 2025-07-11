ec83cf281c86f58e9283f3b2bd309b634d749c9a
deepak-stack18 <hardikgupta4321@gmail.com>
2025-07-11 10:57:37 +0530

    Add files via upload

---
+++ Day 6/LongestConsecutiveSequence.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int count = 1;
+        int max = 1;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i+1] == nums[i]){
+                continue;
+            }
+            if(nums[i+1] - nums[i] == 1){
+                count++;
+            }
+            else{
+                max = Math.max(max, count);
+                count = 1;
+            }
+        }
+        max = Math.max(max, count);
+        return max;
+    }
+}
